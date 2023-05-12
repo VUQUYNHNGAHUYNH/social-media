ef870d582c400096a3b5a0ac59b3a599beb6132e
NGA <r0917268@student.thomasmore.be>
2023-05-12 17:39:30 +0700

    register and login functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -36,4 +36,21 @@ export const register = async (req, res) => {
   }
 };
 
+// Login User
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ error: "User not found" });
 
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res.status(400).json({ error: "Invalid credentials." });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
